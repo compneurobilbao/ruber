b6506d16b1edc93bf22fa0c2201d7b044ca671c3
erramuzpe <asier.erramuzpe@gmail.com>
2017-04-25 11:56:32 +0200

    fmriprep and mriqc docker cmds

---
+++ src/data/fmriprep.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 25 11:35:55 2017
+
+@author: asier
+"""
+from src.env import BIDS_DATA, DATA
+import shutil
+import os
+import os.path as op
+import json
+import subprocess
+
+
+"""
+fmriprep
+"""
+
+DATA_DIR = BIDS_DATA
+OUTPUT_DIR = op.join(DATA, 'processed')
+WORK_DIR = op.join(DATA, 'interim')
+
+docker run -ti  \
+	-v $DATA_DIR:/data:ro \
+	-v $OUTPUT_DIR:/output \
+	-v $WORK_DIR:/work \
+	-w /work \
+	poldracklab/fmriprep:latest \
+	/data /output participant --participant_label sub-001 \
+	-w /work --no-freesurfer 
+
+
+"""
+MRIQC
+"""
+
+DATA_DIR = BIDS_DATA
+OUTPUT_DIR = op.join(DATA, 'processed')
+WORK_DIR = op.join(DATA, 'interim')
+
+
+docker run -ti  \
+	-v $DATA_DIR:/data \
+	-v $OUTPUT_DIR:/output \
+	-v $WORK_DIR:/work \
+	-w /work \
+	poldracklab/mriqc:latest \
+	/data /output participant --participant_label sub-001 \
+	-w /work --verbose-reports
+
+
+
+
+
+
+
