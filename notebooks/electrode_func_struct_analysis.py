fdf67c1c9e2c22fe6be28da3bda9366812483835
erramuzpe <asier.erramuzpe@gmail.com>
2017-05-26 10:30:25 +0200

    order dict. plot struct

--- notebooks/electrode_func_struct_analysis.py
+++ notebooks/electrode_func_struct_analysis.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from src.postproc.utils import load_elec_file
-from src.env import DATA, ATLAS_TYPES, CONFOUNDS_ID
-import os.path as op
+from src.env import DATA, ATLAS_TYPES
+#import os.path as op
 from os.path import join as opj
 import numpy as np
 
 
 SUBJECT_LIST = ['sub-001']
 SESSION_LIST = ['ses-presurg']
+
+
+def order_dict(dictionary):
+    import collections
+    import re
+
+    my_fun = lambda k, v: [k, int(v)]
+
+    ordered = collections.OrderedDict(
+            sorted(dictionary.items(),
+                   key=lambda t: my_fun(*re.match(r'([a-zA-Z]+)(\d+)',
+                                                  t[0]).groups())))
+    return ordered
+
 
 if __name__ == "__main__":
 
@@ -22,6 +36,13 @@ if __name__ == "__main__":
                             'sub-001_elec_2514_closest_rois.roi')
             elec_location_mni09 = load_elec_file(elec_file)
 
+            ordered_elec = order_dict(elec_location_mni09)
+    
+            elec_tags = list(ordered_elec.keys())
+            elec_rois = np.array(list(ordered_elec.values()))[:,0]
+
+            idx = np.ix_(elec_rois, elec_rois)
+            
             # load function (conn matrix?)
             
             
@@ -31,3 +52,23 @@ if __name__ == "__main__":
                               'conmat_' + atlas + '_sc.csv')
             
             struct_mat = np.loadtxt(struct_file, delimiter = ',', skiprows=1)
+
+            struct_mat[idx]
+
+
+
+from matplotlib import pyplot as plt
+
+plt.figure(figsize=(10, 10))
+# Mask the main diagonal for visualization:
+
+plt.imshow(struct_mat[idx], interpolation="nearest", cmap="RdBu_r")
+           #vmax=0.8, vmin=-0.8)
+
+# Add labels and adjust margins
+x_ticks = plt.xticks(range(len(elec_tags)), elec_tags, rotation=90)
+y_ticks = plt.yticks(range(len(elec_tags)), elec_tags)
+plt.gca().yaxis.tick_right()
+plt.subplots_adjust(left=.01, bottom=.3, top=.99, right=.62)
+
+
