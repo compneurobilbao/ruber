ef51418c2ea4e76e968cf6275aa215e3553c6cf8
erramuzpe <asier.erramuzpe@gmail.com>
2017-07-11 12:11:41 +0200

    added utils for electrophys preproc

---
+++ src/elecphys/utils.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Utilities to help in the deep electrode signals pre-processing
+"""
+from src.env import DATA, ATLAS_TYPES
+import os
+from os.path import join as opj
+import nibabel as nib
+import numpy as np
