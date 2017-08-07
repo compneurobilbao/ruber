b42253fd4b962e9bfe959c8fd622381a0ac7ebb7
erramuzpe <asier.erramuzpe@gmail.com>
2017-08-07 15:17:51 +0200

    clean trk script, save tomorrow in utils

--- src/dmri/correct_trk.py
+++ src/dmri/correct_trk.py
@@ -6,7 +6,7 @@ Created on Mon Aug  7 14:28:31 2017
 @author: asier
 """
 
-from nibabel.streamlines import load, is_supported, detect_format, save, Tractogram
+from nibabel.streamlines import save, Tractogram
 from nibabel.streamlines.trk import TrkFile
 
 a = TrkFile.load('/home/asier/git/ruber/data/processed/tract/_session_id_ses-presurg_subject_id_sub-001/tracts.trk')
