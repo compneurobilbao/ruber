edeaa6283c93263719ac2efd29b924bc2e14ae68
erramuzpe <asier.erramuzpe@gmail.com>
2017-08-08 10:08:11 +0200

    dtk pipeline for tracks

--- src/dmri/dtk.py
+++ src/dmri/dtk.py
@@ -11,12 +11,10 @@ from nipype.interfaces.io import SelectFiles, DataSink
 import nipype.pipeline.engine as pe
 import nipype.algorithms.misc as misc
 from nipype.interfaces.utility import IdentityInterface
-from nipype.interfaces.camino import (Image2Voxel, FSL2Scheme, DTIFit, Track,
-                                      Conmat, ComputeFractionalAnisotropy,
-                                      AnalyzeHeader)
-from nipype.interfaces.camino2trackvis import Camino2Trackvis as cam2trk
 
-#from   src.config  import setup_node, check_atlas_file
+import nipype.interfaces.fsl as fsl          # fsl
+import nipype.interfaces.diffusion_toolkit as dtk
+
 from src.utils import (get_datasink,
                        get_interface_node,
                        get_input_node,
@@ -26,7 +24,7 @@ from src.utils import (get_datasink,
                        )
 
 
-def dtk_tractography(wf_name="camino_tract"):
+def dtk_tractography(wf_name="dtk_tract"):
     """ Run the diffusion MRI pre-processing workflow against the diff files in `data_dir`.
 
     Nipype Inputs
@@ -67,35 +65,20 @@ def dtk_tractography(wf_name="camino_tract"):
     -------
     wf: nipype Workflow
     """
-    in_fields  = ["diff", "bvec", "bval", "mask", "atlas_2514", "atlas_2754"]
-    out_fields = ["tensor", "tracks_2514", "tracks_2754","mean_fa", "fa"]
-#     "connectivity_2514","connectivity_2754", "trk_2514", "trk_2754"]
+    in_fields  = ["diff", "bvec", "bval", "mask"]
+    out_fields = ["tensor", "tracks"]
 
     tract_input  = pe.Node(IdentityInterface(fields=in_fields,
                                              mandatory_inputs=True),
                            name="tract_input")
 
-    img2vox_diff = pe.Node(Image2Voxel(out_type="float"), name="img2vox_diff")
-    img2vox_mask = pe.Node(Image2Voxel(out_type="short"), name="img2vox_mask")
-    fsl2scheme   = pe.Node(FSL2Scheme(),                  name="fsl2scheme")
-    dtifit       = pe.Node(DTIFit(),                      name="dtifit")
-    fa           = pe.Node(ComputeFractionalAnisotropy(), name="fa")
+    dtifit = pe.Node(interface=dtk.DTIRecon(),name='dtifit')
 
-    analyzehdr_fa = pe.Node(interface=AnalyzeHeader(), name="analyzeheader_fa")
-    analyzehdr_fa.inputs.datatype = "double"
-
-    fa2nii = pe.Node(interface=misc.CreateNifti(), name='fa2nii')
-
-    track_2514  = pe.Node(Track(inputmodel="dt", out_file="tracts.Bfloat_2514"), name="track_2514")
-    track_2754  = pe.Node(Track(inputmodel="dt", out_file="tracts.Bfloat_2754"), name="track_2754")
-#    conmat_2514 = pe.Node(Conmat(output_root="conmat_atlas_2514_"), name="conmat_2514")
-#    conmat_2754 = pe.Node(Conmat(output_root="conmat_atlas_2754_"), name="conmat_2754")
-#
-#    trk_2514 = pe.Node(cam2trk(voxel_order='LAS',
-#                               min_length=30), name="trk_2514")
-#    trk_2754 = pe.Node(cam2trk(voxel_order='LAS',
-#                               min_length=30), name="trk_2754")
-
+    dtk_tracker = pe.Node(interface=dtk.DTITracker(), name="dtk_tracker")
+    dtk_tracker.inputs.invert_x = True
+    
+    smooth_trk = pe.Node(interface=dtk.SplineFilter(), name="smooth_trk")
+    smooth_trk.inputs.step_length = 0.5
 
     tract_output = pe.Node(IdentityInterface(fields=out_fields),
                            name="tract_output")
@@ -105,67 +88,20 @@ def dtk_tractography(wf_name="camino_tract"):
 
     # Connect the nodes
     wf.connect([
-                # convert data to camino format
-                (tract_input,   img2vox_diff,     [("diff",                  "in_file"     )]),
-                (tract_input,   img2vox_mask,     [("mask",                  "in_file"     )]),
+                # input
+                (tract_input, dtifit, [("diff", "DWI")]),
+                (tract_input, dtifit, [("bval", "bvals")]),
+                (tract_input, dtifit, [("bvec", "bvecs")]),
 
-                # convert bvec and bval to camino scheme
-                (tract_input,   fsl2scheme,       [("bvec",                  "bvec_file"   ),
-                                                   ("bval",                  "bval_file"   )]),
-
-                # dtifit
-                (img2vox_diff,  dtifit,           [("voxel_order",           "in_file"     )]),
-                (img2vox_mask,  dtifit,           [("voxel_order",           "bgmask"      )]),
-                (fsl2scheme,    dtifit,           [("scheme",                "scheme_file" )]),
-
-                # calculate FA
-                (fsl2scheme,    fa,               [("scheme",                "scheme_file" )]),
-                (dtifit,        fa,               [("tensor_fitted",         "in_file"     )]),
-
-                # tractography
-                (tract_input,   track_2514,            [("atlas_2514",                 "seed_file"   )]),
-                (dtifit,        track_2514,            [("tensor_fitted",         "in_file"     )]),
-                # tractography
-                (tract_input,   track_2754,            [("atlas_2754",                 "seed_file"   )]),
-                (dtifit,        track_2754,            [("tensor_fitted",         "in_file"     )]),
-
-
-                # convert FA data to NifTI
-                (fa,            analyzehdr_fa,    [("fa",                    "in_file"     )]),
-                (tract_input,   analyzehdr_fa,    [(('diff', get_vox_dims),  "voxel_dims"  ),
-                                                   (('diff', get_data_dims), "data_dims"   )]),
-
-                (tract_input,   fa2nii,           [(("diff", get_affine),    "affine"      )]),
-                (analyzehdr_fa, fa2nii,           [("header",                "header_file" )]),
-                (fa,            fa2nii,           [("fa",                    "data_file"   )]),
-
-#                # connectivity matrix
-#                (tract_input,   conmat_2514,           [("atlas_2514",                 "target_file" )]),
-#                (track_2514,    conmat_2514,           [("tracked",               "in_file"     )]),
-#                # connectivity matrix
-#                (tract_input,   conmat_2754,           [("atlas_2754",                 "target_file" )]),
-#                (track_2754,    conmat_2754,           [("tracked",               "in_file"     )]),
-
-#                # trk file
-#                (track_2514,    trk_2514,           [("tracked", "in_file")]),
-#                (tract_input,   trk_2514,           [(('diff', get_vox_dims),  "voxel_dims"  ),
-#                                                     (('diff', get_data_dims), "data_dims"   )]),
-                # trk file
-#                (track_2754,    trk_2754,           [("tracked", "in_file")]),
-#                (tract_input,   trk_2754,           [(('diff', get_vox_dims),  "voxel_dims"  ),
-#                                                     (('diff', get_data_dims), "data_dims"   )]),
-
-
+                # dti fit
+                (tract_input, dtk_tracker, [("mask", "mask1_file")]),
+                # tracking and smoothing
+                (dtifit, dtk_tracker, [("tensor", "tensor_file")]),
+                (dtk_tracker, smooth_trk, [('track_file', 'track_file')]),
 
                 # output
-                (fa2nii,        tract_output,     [("nifti_file",            "fa"          )]),
-                (dtifit,        tract_output,     [("tensor_fitted",         "tensor"      )]),
-                (track_2514,         tract_output,     [("tracked",               "tracks_2514"      )]),
-#                (conmat_2514,        tract_output,     [("conmat_sc",             "connectivity_2514")]),
-                (track_2754,         tract_output,     [("tracked",               "tracks_2754"      )]),
-#                (conmat_2754,        tract_output,     [("conmat_sc",             "connectivity_2754")]),
-#                (trk_2514,        tract_output,     [("trackvis",             "trk_2514")]),
-#                (trk_2754,        tract_output,     [("trackvis",             "trk_2754")]),
+                (smooth_trk, tract_output, [("smoothed_track_file", "tracks")]),
+                (dtifit, tract_output, [("tensor", "tensor")]),
               ])
     return wf
 
@@ -217,8 +153,6 @@ def run_dtk_tractography(subject_list, session_list):
                  'bval': 'raw/bids/{subject_id}/{session_id}/dwi/{subject_id}_{session_id}_dwi.bval',
                  'bvec_rotated': 'processed/diff/_session_id_{session_id}_subject_id_{subject_id}/{subject_id}_{session_id}_dwi_rotated.bvec',
                  'brain_mask_diff': 'processed/diff/_session_id_{session_id}_subject_id_{subject_id}/r{subject_id}_{session_id}_T1w_brainmask.nii',
-                 'atlas_diff_2514': 'processed/diff/_session_id_{session_id}_subject_id_{subject_id}/r{subject_id}_{session_id}_atlas_2514.nii',
-                 'atlas_diff_2754': 'processed/diff/_session_id_{session_id}_subject_id_{subject_id}/r{subject_id}_{session_id}_atlas_2754.nii',
                  }
     selectfiles = pe.Node(SelectFiles(templates,
                                       base_directory=DATA),
@@ -229,7 +163,7 @@ def run_dtk_tractography(subject_list, session_list):
                        name="datasink")
 
     # The workflow box
-    tract_wf = camino_tractography()
+    tract_wf = dtk_tractography()
 
     wf = pe.Workflow(name='artifact')
     wf.base_dir = working_dir
@@ -241,20 +175,11 @@ def run_dtk_tractography(subject_list, session_list):
                                          ("brain_mask_diff", "tract_input.mask"),
                                          ("eddy_corr_file",  "tract_input.diff"),
                                          ("bvec_rotated",    "tract_input.bvec"),
-                                         ("atlas_diff_2514", "tract_input.atlas_2514"),
-                                         ("atlas_diff_2754", "tract_input.atlas_2754")
                                          ]),
 
                 # output
-                (tract_wf, datasink, [("tract_output.tensor",       "tract.@tensor"),
-                                      ("tract_output.tracks_2514",       "tract.@tracks_2514"),
-                                      ("tract_output.tracks_2754",       "tract.@tracks_2754"),
-#                                      ("tract_output.connectivity_2514", "tract.@connectivity_2514"),
-#                                      ("tract_output.connectivity_2754", "tract.@connectivity_2754"),
-                                      ("tract_output.mean_fa",      "tract.@mean_fa"),
-                                      ("tract_output.fa",           "tract.@fa"),
-#                                      ("tract_output.trk_2514",           "tract.@trk_2514"),
-#                                      ("tract_output.trk_2754",           "tract.@trk_2754"),
+                (tract_wf, datasink, [("tract_output.tensor",       "tract.@tensor_dtk"),
+                                      ("tract_output.tracks",       "tract.@tracks_dtk"),
                                       ])
                 ])
 
