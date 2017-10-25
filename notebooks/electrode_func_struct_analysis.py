ce2708d39961ebd97241e6476180da629714f8a8
erramuzpe <asier.erramuzpe@gmail.com>
2017-10-25 12:17:59 +0200

    added plot2

--- notebooks/electrode_func_struct_analysis.py
+++ notebooks/electrode_func_struct_analysis.py
@@ -54,7 +54,7 @@ def plot_matrix(matrix, elec_tags, log=False):
     plt.subplots_adjust(left=.01, bottom=.3, top=.99, right=.62)
 
 
-if __name__ == "__main__":
+def figures_1():
 
     rithms = ['filtered', 'delta', 'theta', 'alpha', 'beta', 'gamma', 'gamma_high']
     SUBJECT_LIST = ['sub-001', 'sub-002', 'sub-003', 'sub-004']
@@ -158,4 +158,113 @@ if __name__ == "__main__":
                           '_bands.pdf'),
                           figures,
                           dpi=250)
-      
+
+
+def figures_2():
+
+    rithms = ['filtered', 'delta', 'theta', 'alpha', 'beta', 'gamma', 'gamma_high']
+    SUBJECT_LIST = ['sub-001', 'sub-002', 'sub-003', 'sub-004']
+    SESSION_LIST = ['ses-presurg']
+
+    sub_ses_comb = [[subject, session] for subject in SUBJECT_LIST
+                    for session in SESSION_LIST]
+
+    sphere = 3
+    denoise_type = 'gsr'
+
+    for i, sub_ses in enumerate(sub_ses_comb):
+        sub, ses = sub_ses
+        output_dir_path = opj(CWD, 'reports', 'figures', sub)
+        if not os.path.exists(output_dir_path):
+            os.makedirs(output_dir_path)
+        # FUNCTION MATRIX
+        # load function (conn matrix?)
+        func_file = opj(DATA, 'processed', 'fmriprep', sub, ses, 'func',
+                        'time_series_noatlas_' + denoise_type + '_' +
+                        str(sphere) + '.txt')
+        func_mat = np.loadtxt(func_file)
+
+        correlation_measure = ConnectivityMeasure(kind='correlation')
+        corr_mat = correlation_measure.fit_transform([func_mat])[0]
+
+        # STRUCT MATRIX
+        struct_mat = np.load(opj(DATA, 'raw', 'bids', sub, 'electrodes', ses,
+                                 'con_mat_noatlas_' +
+                                 str(sphere) + '.npy'))
+
+        th = 10
+        struct_mat = struct_mat
+        struct_mat_treatment = 'no_log_' + str(th)
+
+        idx = np.where(struct_mat >= th)
+        struct_mat = struct_mat[idx]
+        corr_mat = corr_mat[idx]
+
+        # Electrophysiology
+        for elec_reg_type in ['regressed', 'not_regressed']:
+            input_path = opj(CWD, 'data', 'processed', 'elec_record',
+                             sub, 'interictal_' + elec_reg_type)
+            figures = []
+            corr_values_struct = []
+            corr_values_func = []
+
+            for rithm in rithms:
+                # load random file
+                random_data = np.load(opj(input_path, 'alpha',
+                                          'interictal_1.npy'))
+                contact_num = random_data.shape[1]
+                all_conn_mat = np.zeros((12, contact_num, contact_num))
+
+                files = [file for file in os.listdir(opj(input_path, rithm))
+                         if file.endswith('npy')]
+                for i, file in enumerate(files):
+                    elec_data = np.load(opj(input_path, rithm, file))
+
+                    elec_conn_mat = np.zeros((contact_num, contact_num))
+                    elec_conn_mat = np.corrcoef(elec_data.T)
+                    all_conn_mat[i, :, :] = elec_conn_mat
+                # Get elec con_mat
+                con_mat = np.mean(all_conn_mat, 0)
+
+                # scatter vs struct
+                corr_value = np.corrcoef(np.ndarray.flatten(struct_mat),
+                                         np.ndarray.flatten(corr_mat))[0][1]
+                corr_values_struct.append(corr_value)
+
+                plt.scatter(struct_mat, con_mat[idx])
+                ax = plt.title('R = ' + str(corr_value) +
+                               ' #Streamlines vs corr values of ' + rithm +
+                               ' ' + elec_reg_type)
+                plt.xlabel(struct_mat_treatment)
+                plt.ylabel('elec corr')
+                fig = ax.get_figure()
+                figures.append(fig)
+                plt.close()
+
+                # scatter vs func
+                corr_value = np.corrcoef(np.ndarray.flatten(struct_mat),
+                                         np.ndarray.flatten(corr_mat))[0][1]
+                corr_values_func.append(corr_value)
+
+                plt.scatter(corr_mat, con_mat[idx])
+                ax = plt.title('Func corr vs corr values of ' + rithm +
+                               ' ' + elec_reg_type)
+                plt.xlabel('Func corr')
+                plt.ylabel('elec corr')
+                fig = ax.get_figure()
+                figures.append(fig)
+                plt.close()
+
+            multipage(opj(output_dir_path,
+                          'Scatter_' + elec_reg_type + ' ' +
+                          struct_mat_treatment + ' ' +
+                          '_bands.pdf'),
+                      figures,
+                      dpi=250)
+            plt.close("all")
+
+            np.save(opj(CWD, 'reports', 'stats',
+                        'stats_' + sub + '_' +
+                        struct_mat_treatment + '_' +
+                        elec_reg_type),
+                    np.array(corr_values_struct))
