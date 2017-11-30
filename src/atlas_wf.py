7eb76bde618e45c4cd8ee862853bda70a502f5f7
erramuzpe <asier.erramuzpe@gmail.com>
2017-11-30 11:11:57 +0100

    experiment no zscore

--- src/atlas_wf.py
+++ src/atlas_wf.py
@@ -23,7 +23,7 @@ from src.postproc.utils import (t1w_electrodes_to_09c,
                                 export_data_to_mat,
                                 )
 
-SUBJECT_LIST = ['sub-002','sub-003', 'sub-004']
+SUBJECT_LIST = ['sub-001','sub-002','sub-003', 'sub-004']
 SESSION_LIST = ['ses-presurg']
 
 """
