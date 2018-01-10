debfd4e420b651c4e258848e2a13a0eb8ba22707
erramuzpe <asier.erramuzpe@gmail.com>
2018-01-10 16:28:08 +0100

    style corrections

--- src/elecphys/active_state_functions.py
+++ src/elecphys/active_state_functions.py
@@ -10,65 +10,64 @@ RAW_ELEC = opj(DATA, 'raw', 'elec_record')
 PROCESSED_ELEC = opj(DATA, 'processed', 'elec_record')
 
 SUBJECTS = ['sub-001', 'sub-002', 'sub-003', 'sub-004']
-RITHMS = ['prefiltered', 'filtered', 'delta', 'theta', 'alpha', 'beta', 'gamma', 'gamma_high']
+RITHMS = ['prefiltered', 'filtered', 'delta', 'theta',
+          'alpha', 'beta', 'gamma', 'gamma_high']
 
 
 def active_state_to_signal(as_data, real_data):
     """
-    Takes real data matrix and active state data in Paolo's format. 
+    Takes real data matrix and active state data in Paolo's format.
     Creates a 0-1 matrix with Paolo's info and filters the real data with it.
-    
-    The output data is the signal itself, with the non active state points removed
+
+    The output is the signal itself, with the non active state points removed
     """
-    
+
     num_elec = as_data.shape[1]
     output_data = np.zeros((30000, num_elec))
-    
+
     for elec_idx in range(num_elec):
         for pair in as_data[0][elec_idx]:
             output_data[pair[0]-1:pair[1], elec_idx] = 1
-        
+
         output_data[:, elec_idx] = (output_data[:, elec_idx] *
                                     real_data[:, elec_idx])
-        
+
     return output_data
 
 
 def create_active_state_records():
-    
+
     for sub in SUBJECTS:
-        
         # create active state folder for sub
         sub_dir = opj(PROCESSED_ELEC, sub, 'active_state')
         if not os.path.exists(sub_dir):
             os.makedirs(sub_dir)
-            
+
         for rit_num_code, rit in enumerate(RITHMS, 1):
-            
+
             if rit == 'prefiltered':
                 continue
             # create rithm folder in sub
             rithm_dir = opj(sub_dir, rit)
             if not os.path.exists(rithm_dir):
                 os.makedirs(rithm_dir)
-                
-            for chunk in range (1, 13): # 12 chunks of 1 sec
+
+            for chunk in range(1, 13):  # 12 chunks of 1 sec
 
                 # load real data sub/rithm
                 file = opj(PROCESSED_ELEC, sub, 'interictal_not_regressed',
                            rit, 'interictal_' + str(chunk) + '.npy')
                 real_data = np.load(file)
-                          
+
                 # load AS data
                 file = opj(RAW_ELEC, sub, 'interictal',
-                           'interictal_' + str(chunk) + '_fb_' + 
+                           'interictal_' + str(chunk) + '_fb_' +
                            str(rit_num_code) + '_numStd_2.mat')
                 as_data = np.array(sio.loadmat(file).get('activeState'))
 
-                
                 as_signal = active_state_to_signal(as_data, real_data)
-                        
-                output_file = opj(rithm_dir, 'active_state_' + str(chunk) + '.npy')
+
+                output_file = opj(rithm_dir,
+                                  'active_state_' + str(chunk) + '.npy')
                 np.save(output_file, as_signal)
-    
     return
